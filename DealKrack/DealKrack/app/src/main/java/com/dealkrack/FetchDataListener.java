2d7e86d045a63b0e58304a7b79aea5aeb02a4ad0
Hitesh <hitesh9725682023@gmail.com>
2016-06-16 02:37:58 -0700

    fgdffdg

--- DealKrack/DealKrack/app/src/main/java/com/dealkrack/FetchDataListener.java
+++
@@ -1,8 +0,0 @@
-package com.dealkrack;
-
-import java.util.List;
-
-public interface FetchDataListener {
-    public void onFetchComplete(List<Application> data);
-    public void onFetchFailure(String msg);
-}
